fb7745e319c3dda997fc703752edcbce05d9979f
ret2home <defineprogram@gmail.com>
2024-06-29 00:11:11 +0900

    formatted by ruff

--- src/crawling.py
+++ src/crawling.py
@@ -2,32 +2,72 @@ import json
 import requests
 from lib import wave_data
 
+
 # 都道府県データをダウンロードする
 def crawl(prefecture: str):
     print(prefecture)
-    url=f"https://tsukatte.com/wp-content/uploads/2022/11/{prefecture}-prefecture_map_outline_white.png"
-    if prefecture=="fukui":
-        url="https://tsukatte.com/wp-content/uploads/2022/11/fukui-prefecture_map_white.png"
-    data=requests.get(url).content
-    with open(f"./prefectures/{prefecture}.png",mode="wb")as f:
+    url = f"https://tsukatte.com/wp-content/uploads/2022/11/{prefecture}-prefecture_map_outline_white.png"
+    if prefecture == "fukui":
+        url = "https://tsukatte.com/wp-content/uploads/2022/11/fukui-prefecture_map_white.png"
+    data = requests.get(url).content
+    with open(f"./prefectures/{prefecture}.png", mode="wb") as f:
         f.write(data)
         f.close()
 
+
 if __name__ == "__main__":
-    prefectures=[
+    prefectures = [
         "okinawa",
-        "kagoshima","miyazaki","oita","kumamoto","nagasaki","saga","fukuoka",
-        "kochi","ehime","kagawa","tokushima",
-        "yamaguchi","hiroshima","okayama","shimane","tottori",
-        "wakayama","nara","hyogo","osaka","kyoto","shiga","mie",
-        "aichi","shizuoka","gifu","nagano","yamanashi","ishikawa","fukui","toyama","niigata",
-        "kanagawa","tokyo","chiba","saitama","gunma","tochigi","ibaraki",
-        "fukushima","yamagata","akita","miyagi","iwate","aomori",
-        "hokkaido"
+        "kagoshima",
+        "miyazaki",
+        "oita",
+        "kumamoto",
+        "nagasaki",
+        "saga",
+        "fukuoka",
+        "kochi",
+        "ehime",
+        "kagawa",
+        "tokushima",
+        "yamaguchi",
+        "hiroshima",
+        "okayama",
+        "shimane",
+        "tottori",
+        "wakayama",
+        "nara",
+        "hyogo",
+        "osaka",
+        "kyoto",
+        "shiga",
+        "mie",
+        "aichi",
+        "shizuoka",
+        "gifu",
+        "nagano",
+        "yamanashi",
+        "ishikawa",
+        "fukui",
+        "toyama",
+        "niigata",
+        "kanagawa",
+        "tokyo",
+        "chiba",
+        "saitama",
+        "gunma",
+        "tochigi",
+        "ibaraki",
+        "fukushima",
+        "yamagata",
+        "akita",
+        "miyagi",
+        "iwate",
+        "aomori",
+        "hokkaido",
     ]
-    data={}
+    data = {}
     for prefecture in prefectures:
         crawl(prefecture)
-        data[prefecture]=wave_data(f"./prefectures/{prefecture}.png")
-    with open("./data.json","w") as f:
-        json.dump(data,f)
+        data[prefecture] = wave_data(f"./prefectures/{prefecture}.png")
+    with open("./data.json", "w") as f:
+        json.dump(data, f)
--- src/main.py
+++ src/main.py
@@ -2,34 +2,74 @@ import sys
 import json
 from lib import wave_data, cos_similarity
 
-# 類似度を各都道府県について計算する
-def similar_ranking(file_path,prefectures,wv_pres,show=False):
-    wv=wave_data(file_path,show)
 
-    results=[]
+# 類似度を各都道府県について計算する
+def similar_ranking(file_path, prefectures, wv_pres, show=False):
+    wv = wave_data(file_path, show)
+
+    results = []
     for prefecture in prefectures:
-        results.append((cos_similarity(wv,wv_pres[prefecture]),prefecture))
-    results=sorted(results)
+        results.append((cos_similarity(wv, wv_pres[prefecture]), prefecture))
+    results = sorted(results)
     return results
 
+
 if __name__ == "__main__":
-    prefectures=[
+    prefectures = [
         "okinawa",
-        "kagoshima","miyazaki","oita","kumamoto","nagasaki","saga","fukuoka",
-        "kochi","ehime","kagawa","tokushima",
-        "yamaguchi","hiroshima","okayama","shimane","tottori",
-        "wakayama","nara","hyogo","osaka","kyoto","shiga","mie",
-        "aichi","shizuoka","gifu","nagano","yamanashi","ishikawa","fukui","toyama","niigata",
-        "kanagawa","tokyo","chiba","saitama","gunma","tochigi","ibaraki",
-        "fukushima","yamagata","akita","miyagi","iwate","aomori",
-        "hokkaido"
+        "kagoshima",
+        "miyazaki",
+        "oita",
+        "kumamoto",
+        "nagasaki",
+        "saga",
+        "fukuoka",
+        "kochi",
+        "ehime",
+        "kagawa",
+        "tokushima",
+        "yamaguchi",
+        "hiroshima",
+        "okayama",
+        "shimane",
+        "tottori",
+        "wakayama",
+        "nara",
+        "hyogo",
+        "osaka",
+        "kyoto",
+        "shiga",
+        "mie",
+        "aichi",
+        "shizuoka",
+        "gifu",
+        "nagano",
+        "yamanashi",
+        "ishikawa",
+        "fukui",
+        "toyama",
+        "niigata",
+        "kanagawa",
+        "tokyo",
+        "chiba",
+        "saitama",
+        "gunma",
+        "tochigi",
+        "ibaraki",
+        "fukushima",
+        "yamagata",
+        "akita",
+        "miyagi",
+        "iwate",
+        "aomori",
+        "hokkaido",
     ]
     with open("./data.json") as f:
-        wv_pres=json.load(f)
-        
-    res=similar_ranking(sys.argv[1],prefectures,wv_pres,True)
+        wv_pres = json.load(f)
+
+    res = similar_ranking(sys.argv[1], prefectures, wv_pres, True)
 
     # 上位候補 5 つの都道府県を出力
-    print(f"result:")
+    print("result:")
     for i in range(5):
-        print(res[i][1],100-res[i][0]/90*100)
+        print(res[i][1], 100 - res[i][0] / 90 * 100)
