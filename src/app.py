eb0a894fd7799ce6b239a0c1dea7425cb1ebb31c
ret2home <defineprogram@gmail.com>
2024-07-03 00:13:35 +0900

    test3

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello!!'
+    return json.dumps(json.load(open("./prefectures.json")))
 
 if __name__ == '__main__':
     app.run()
