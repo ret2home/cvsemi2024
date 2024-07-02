b7208154f8e13839fd555bc89fb42000cc6178b9
ret2home <defineprogram@gmail.com>
2024-07-03 00:14:54 +0900

    test!

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return json.dumps(json.load(open("./prefectures.json")))
+    return json.dumps(json.load(open("../prefectures.json")))
 
 if __name__ == '__main__':
     app.run()
